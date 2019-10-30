bbb9f944bcf895d8ae5a42343e08a662d68711a7
Pieter Hynam <pieter@Pieters-MacBook-Pro.local>
2019-10-30 13:16:09 -0700

    Note size calculation.

--- modules/sequencer/PianoRollView.js
+++ modules/sequencer/PianoRollView.js
@@ -1,12 +1,21 @@
 class PianoRollView extends View
 {
     dragThresholdPx = 5;
-    currentNote;
 
     constructor(selector)
     {
         super(selector);
         this._bindHandlers();
+
+        // TODO: Throwaway
+        this.notes = [
+            [1,1.25,60,127]
+        ];
+
+        this.gridResolutionX = 16;
+
+
+        this.renderNotes();
     }
 
     handlers = {
@@ -14,23 +23,55 @@ class PianoRollView extends View
         '.piano-roll__note:mouseup': this.onNoteMouseup
     }
 
+    /**
+     * Handler methods
+     */
     onNoteMousedown(e, el) {
         if(e.offsetX < this.dragThresholdPx || e.offsetX > el.offsetWidth - this.dragThresholdPx) {
-            document.addEventListener('mousemove', this.resizeNote, false);
+            document.addEventListener('mousemove', this.onNoteResize, false);
         }
     }
 
-    onNoteMouseup(e, el) {
-        console.log('up');
-        document.removeEventListener('mousemove', this.resizeNote, false);
+    onNoteMouseup(e) {
+        document.removeEventListener('mousemove', this.onNoteResize, false);
     }
 
-    resizeNote = (e) => {
-        console.log(e.offsetX);
+    onNoteResize(e) {
+
     }
 
-    render = () => {
-        // Iterate over all the notes in array and render their positions to the DOM
+    /**
+     * Render methods
+     */
+    renderNotes = () => {
+        let template = '';
+        for(let i = 0; i < this.notes.length; i++) {
+
+            let note = this.notes[i];
+
+            // Convert start point to position
+
+            template += this.noteTemplate(
+                this.asPercentage(note[1] - note[0]), 
+                this.asPercentage(note[0] - 1)
+            );
+        }
+
+        this.el.innerHTML = template;
+    }
+
+    /**
+     * Template methods
+     */
+    noteTemplate = (width = 0, offsetLeft = 0, offsetTop = 0) => {
+        return `<div class="piano-roll__note" role="button" style="width:${width}%; left:${offsetLeft}%; top:${offsetTop}px"></div>`;
+    }
+
+    /**
+     * Helper methods
+     */
+    asPercentage = (x) => {
+        return x / this.gridResolutionX * 100;
     }
 }
 
